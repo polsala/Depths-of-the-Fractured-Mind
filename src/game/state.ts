73083d41b1571401a4f8201078bbf8edcf32ddb4
Pol Sala <polsalalidon@gmail.com>
2025-12-06 15:27:45 +0100

    main

--- src/game/state.ts
+++ src/game/state.ts
@@ -69,8 +69,8 @@ export function createInitialGameState(): GameState {
     },
     location: {
       depth: 1,
-      x: 0,
-      y: 0,
+      x: 1,
+      y: 1,
     },
     mode: "title",
     flags: {
